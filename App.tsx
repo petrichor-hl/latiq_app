06aba80c382c8f1bcc782054e9e0fea358ea1131
petrichor-hl <tl.hoanglam174@gmail.com>
2024-09-23 22:21:54 +0700

    feat: setup safe-area

--- App.tsx
+++ App.tsx
@@ -1,27 +1,71 @@
 import React, { useEffect } from 'react';
-import { Appearance, Button, Text, View } from 'react-native';
+import {
+  Appearance,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+import { useKeyboardAwareInsets } from './app/base/keyboard/use-keyboard-aware-insets.hook';
 
 function App(): React.JSX.Element {
   useEffect(() => Appearance.setColorScheme('light'), []);
 
-  const callApi = () => {
-    const fetchPokemonData = async () => {
-      try {
-        await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchPokemonData();
-  };
-
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>App Screen</Text>
-      <Button onPress={callApi} title="Test Call API" />
-    </View>
+    <SafeAreaProvider>
+      <HomeScreen />
+    </SafeAreaProvider>
   );
 }
+
+const HomeScreen = () => {
+  const [number, onChangeNumber] = React.useState('');
+  const insets = useSafeAreaInsets();
+
+  return (
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+      }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Button onPress={() => Keyboard.dismiss()} title="dismiss keyboard" />
+      </View>
+      <View
+        style={{
+          padding: 14,
+          backgroundColor: '#00F8',
+          paddingBottom: Math.max(insets.bottom, useKeyboardAwareInsets()) + 14,
+        }}>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 14,
+    color: 'black',
+    backgroundColor: '#E8E8E8',
+    borderRadius: 8,
+  },
+});
 
 export default App;
---
+++ app/base/keyboard/use-keyboard-aware-insets.hook.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Keyboard } from 'react-native';
+
+export const useKeyboardAwareInsets = () => {
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const showSubscription = Keyboard.addListener('keyboardWillShow', e =>
+      setKeyboardHeight(e.endCoordinates.height),
+    );
+    const hideSubscription = Keyboard.addListener('keyboardWillHide', () => {
+      setKeyboardHeight(0);
+    });
+
+    return () => {
+      showSubscription.remove();
+      hideSubscription.remove();
+    };
+  }, []);
+
+  return keyboardHeight;
+};
